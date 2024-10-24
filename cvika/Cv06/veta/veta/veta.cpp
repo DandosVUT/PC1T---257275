a8244dfb9d96c10c716fc8b99ea2788617c01623
Daniel Zeman <257275@vutbr.cz>
2024-10-24 11:15:29 +0200

    Cv06 veta

---
+++ cvika/Cv06/veta/veta/veta.cpp
@@ -0,0 +1,48 @@
+﻿// veta.cpp : Defines the entry point for the application.
+//
+
+#include "veta.h"
+#include <ctype.h>
+
+using namespace std;
+
+int pismena(char *veta)
+{
+	int i;
+	int pocet = 0;
+	for (i = 0; i <= strlen(veta); i++)
+	{
+		if (isalpha(veta[i]) != 0)
+		{
+			pocet += 1;
+		}
+	}
+	printf("pocet pismen v textu je: %d\n", pocet);
+
+	return pocet;
+}
+
+int cisla(char* veta)
+{
+	int i;
+	int pocet = 0;
+	for (i = 0; i <= strlen(veta); i++)
+	{
+		if (isdigit(veta[i]) != 0)
+		{
+			pocet += 1;
+		}
+	}
+	printf("pocet cisel v textu je: %d\n", pocet);
+
+	return pocet;
+}
+
+int main()
+{
+	char text[] = "Toto jsou 2 vzorove vety, ktere budou zpracovavany Vasemi funkcemi. Auto Hyundai ix35 ma pres 120 konskych sil.";
+	pismena(text);
+	cisla(text);
+
+	return 0;
+}
