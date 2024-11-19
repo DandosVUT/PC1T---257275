320fd747459973f5ed15798569385c61ef8c1800
Daniel Zeman <257275@vutbr.cz>
2024-11-19 13:17:09 +0100

    Cv10

--- cvika/Cv10/prace_se_soubory/prace_se_soubory.cpp
+++ cvika/Cv10/prace_se_soubory/prace_se_soubory.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	FILE* vstup;
-	const char* soubor = "D:\\vstup.txt";
+	const char* soubor = "C:/Users/student.DESKTOP-P9NC5QO/Documents/PC1T---257275/cvika/Cv10/prace_se_soubory/vstup.txt";
 
 	if (fopen_s(&vstup, soubor, "r") != 0)
 	{
@@ -19,6 +19,9 @@ int main()
 	char veta[100];
 	int pocet_pismen = 0;
 	int pocet_cisel = 0;
+	int delka_slova[100];
+	for (int i = 0; i < 100; i++)
+		delka_slova[i] = 0;
 
 	while (!feof(vstup))
 	{
@@ -30,10 +33,27 @@ int main()
 			if (isdigit(veta[i]))
 				pocet_cisel += 1;
 		}
+	}
 
+	while (getc(vstup) != EOF)
+	{
+		if (getc(vstup) != 20)
+			delka_slova[i] += 1;
 	}
 	printf("pocet pismen je: %d\n", pocet_pismen);
-	printf("pocet cisel je: %d", pocet_cisel);
+	printf("pocet cisel je: %d\n", pocet_cisel);
+	printf("delky jednotlivych slov jsou: \n");
+
+	fprintf(vstup, "delky jednotlivych slov jsou: \n");
+	fprintf(vstup, "pocet pismen je: %d\n", pocet_pismen);
+	fprintf(vstup, "pocet cisel je: %d\n", pocet_cisel);
+
+	for (int i = 0; i < 100; i++)
+		if (delka_slova[i] > 0)
+		{
+			printf(", %d", delka_slova[i]);
+			fprintf(vstup, ", %d", delka_slova[i]);
+		}
 
 	fclose(vstup);
 
